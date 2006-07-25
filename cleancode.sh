1e20fb13379a3e01956225c6c379fb9fbc3ebe0e
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2006-07-25 08:13:22 +0200

    A simplified cleancode script.

--- cleancode.sh
+++ cleancode.sh
@@ -1,14 +1,6 @@
+echo Cleaning python code in \'`pwd`\' and subdirectories
 for file in `find * | grep "\.py$"`; do
-  echo ${file}
-  if grep -I -m 1 $'	' ${file}; then
-    echo Tabs found in file ${file}!!
-    #exit 1
-  fi
-  sed -i -e $'s/	/    /' ${file}
-  if egrep -I -m 1 $'[ 	]+$' ${file}; then
-    echo Trailing whitespace found in file ${file}!!
-    #exit 1
-  fi
+  sed -i -e $'s/\t/    /' ${file}
   sed -i -e $'s/[ \t]\+$//' ${file}
 done
 exit 0
