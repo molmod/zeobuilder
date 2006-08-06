b38ea58f99efba8b37ece055356c2568a4e799e5
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2006-08-07 00:28:06 +0200

    Even cleaner.

--- cleanfiles.sh
+++ cleanfiles.sh
@@ -1,14 +1,17 @@
+#!/usr/bin/bash
 for i in `find * | egrep "\.pyc$|\.py~$|\.pyc~$|\.bak$"` ; do rm -v ${i}; done
 
 rm -v test/zeobuilder
 rm -vr test/output
 rm -v test/*.so
 rm -v test/zeobuilder_script.py
+rm -v test/ZEOBUILDER-DIALOG-*
 
 rm -v debug/zeobuilder
 rm -vr debug/output
 rm -v debug/*.so
 rm -v debug/zeobuilder_script.py
+rm -v debug/ZEOBUILDER-DIALOG-*
 
 rm -vr extensions/build
 rm -v extensions/*.so
