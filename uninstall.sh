0dced02bc9d17f69ac58c181426ac18c4907733b
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2009-04-29 16:40:05 +0200

    Updated install scripts

--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,9 @@
 # This is a very simplistic uninstall scipt. Use with care!
 
 if [ -n $1 ] && [ "$1" = "--system" ]; then
-  rm -v /usr/bin/zeobuilder
-  rm -vr /usr/share/zeobuilder
-  rm -vr /usr/lib/python*/site-packages/zeobuilder
+  rm -v /usr/local/bin/zeobuilder
+  rm -vr /usr/local/share/zeobuilder
+  rm -vr /usr/local/lib/python*/site-packages/zeobuilder
 else
   if [ -z $PYTHONPATH ]; then
     echo 'WARNING: $PYTHONPATH is not defined, defaulting to \$HOME/lib/python'
