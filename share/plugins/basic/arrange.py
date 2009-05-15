0035d827d6fc7aa3a88e186a1f7d9311a1239de2
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2009-05-15 11:11:33 +0200

    Updated license with mandatory references

--- share/plugins/basic/arrange.py
+++ share/plugins/basic/arrange.py
@@ -1,5 +1,7 @@
 # Zeobuilder is an extensible GUI-toolkit for molecular model construction.
-# Copyright (C) 2007 - 2008 Toon Verstraelen <Toon.Verstraelen@UGent.be>
+# Copyright (C) 2007 - 2009 Toon Verstraelen <Toon.Verstraelen@UGent.be>, Center
+# for Molecular Modeling (CMM), Ghent University, Ghent, Belgium; all rights
+# reserved unless otherwise stated.
 #
 # This file is part of Zeobuilder.
 #
@@ -7,6 +9,16 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
+#
+# In addition to the regulations of the GNU General Public License,
+# publications and communications based in parts on this program or on
+# parts of this program are required to cite the following article:
+#
+# "ZEOBUILDER: a GUI toolkit for the construction of complex molecules on the
+# nanoscale with building blocks", Toon Verstraelen, Veronique Van Speybroeck
+# and Michel Waroquier, Journal of Chemical Information and Modeling, Vol. 48
+# (7), 1530-1541, 2008
+# DOI:10.1021/ci8000748
 #
 # Zeobuilder is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -294,7 +306,5 @@ actions = {
     "SeparateFrame": SeparateFrame,
     "SwapVector": SwapVector,
 }
-
-
 
 
--- test/actions.py
+++ test/actions.py
@@ -1,5 +1,7 @@
 # Zeobuilder is an extensible GUI-toolkit for molecular model construction.
-# Copyright (C) 2007 - 2008 Toon Verstraelen <Toon.Verstraelen@UGent.be>
+# Copyright (C) 2007 - 2009 Toon Verstraelen <Toon.Verstraelen@UGent.be>, Center
+# for Molecular Modeling (CMM), Ghent University, Ghent, Belgium; all rights
+# reserved unless otherwise stated.
 #
 # This file is part of Zeobuilder.
 #
@@ -7,6 +9,16 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
+#
+# In addition to the regulations of the GNU General Public License,
+# publications and communications based in parts on this program or on
+# parts of this program are required to cite the following article:
+#
+# "ZEOBUILDER: a GUI toolkit for the construction of complex molecules on the
+# nanoscale with building blocks", Toon Verstraelen, Veronique Van Speybroeck
+# and Michel Waroquier, Journal of Chemical Information and Modeling, Vol. 48
+# (7), 1530-1541, 2008
+# DOI:10.1021/ci8000748
 #
 # Zeobuilder is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -1291,8 +1303,5 @@ class PrimitiveActions(ApplicationTestCase):
             p.undo()
             self.assert_(universe.extra["foo"] == "bar")
         self.run_test_application(fn)
-
-
-
 
 
